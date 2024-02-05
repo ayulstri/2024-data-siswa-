43f610e259e06c9c87a91cb8076341bb7cc3c9b6
ayulstri <ayulstri175@gmail.com>
2024-02-05 11:08:53 +0700

    update

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-const ad = getFirestore(app);
+const db = getFirestore(app);
 
 export async function ambilDaftarSiswa () {
   const siswaRef = collection(db, "siswa");
